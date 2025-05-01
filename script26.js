2274d81d5d529340acfaf39ea2e88d89444a359b
ho3ein81 <hossein.vali2001@gmail.com>
2025-05-01 13:22:12 +0330

    mymodule

--- script26.js
+++ script26.js
@@ -1,22 +1,62 @@
 const theTimer = document.querySelector(".timer");
+const testArea = document.querySelector("#test-area");
+
+
+
+
 var timer=[0,0,0,0];
+var timerRunning=false
+
+
+function leetingzero(time){
+    if(time<=9){
+        time="0"+time;
+
+    }
+    return time;
+}
+
 
 
 function startTimer(){
-    let CurrentTimer = timer[0]+":"+timer[1]+":"+timer[2];
+    let CurrentTimer = leetingzero(timer[0])+":"+leetingzero(timer[1])+":"+leetingzero(timer[2]);
 
     theTimer.innerHTML= CurrentTimer;
 
     timer[3]++;
    
    timer[0]=Math.floor(timer[3]/100 /60);
-   timer[1]=Math.floor(timer[3]/100);-(timer[0]*60);
+   timer[1]=Math.floor(timer[3]/100)-(timer[0]*60);
    timer[2]=Math.floor(timer[3]-(timer[1]*100)-(timer[0]*6000));
 
 
 }
 
-setInterval(startTimer,10);
+// 2 ta shart gozashtim ta timer dorost kar kone va age masalan chand bar 
+//  timer az 0 shoroo be kar kard soraatesh ziad nashe 
+// bed dalil chnad bar farakhani function
+
+
+function Start(){
+    let textEnteredLength =testArea.value.length;
+    if(textEnteredLength==0 && !timerRunning)
+        {
+
+   setInterval(startTimer,10); 
+   timerRunning=true;   
+}
+
+}
+
+
+
+
+
+testArea.addEventListener("keypress",Start);
+
+
+
+
 
 
 
