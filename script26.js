602ef8ff26d311725b91d3172adebc5c4b99171a
ho3ein81 <hossein.vali2001@gmail.com>
2025-05-03 13:32:49 +0330

    commit2

--- script26.js
+++ script26.js
@@ -1,12 +1,12 @@
 const theTimer = document.querySelector(".timer");
 const testArea = document.querySelector("#test-area");
-
-
-
+const originText = document.querySelector("#origin-text p").innerHTML;
+const testwrapper = document.querySelector(".test-wrapper")
+const resetButton = document.querySelector("#reset")
 
 var timer=[0,0,0,0];
-var timerRunning=false
-
+var timerRunning=false;
+var interval;
 
 function leetingzero(time){
     if(time<=9){
@@ -19,6 +19,7 @@ function leetingzero(time){
 
 
 function startTimer(){
+
     let CurrentTimer = leetingzero(timer[0])+":"+leetingzero(timer[1])+":"+leetingzero(timer[2]);
 
     theTimer.innerHTML= CurrentTimer;
@@ -32,9 +33,60 @@ function startTimer(){
 
 }
 
+
+
+
+function spellCheck(){
+    let textEntered=testArea.value;
+    let originTextMatch=originText.substring(0,textEntered.length);
+
+    if(textEntered==originText){
+        
+        testwrapper.style.borderColor="green"
+        clearInterval(interval)
+
+
+    }else{
+        if(textEntered==originTextMatch){
+            
+            testwrapper.style.borderColor="yellow";
+            
+
+        }
+        else{
+
+            testwrapper.style.borderColor="red";
+            
+
+        }
+    }
+
+
+
+}
+
+
+
+
 // 2 ta shart gozashtim ta timer dorost kar kone va age masalan chand bar 
 //  timer az 0 shoroo be kar kard soraatesh ziad nashe 
 // bed dalil chnad bar farakhani function
+function reset(){
+    clearInterval(interval);
+    interval=null;
+    timer=[0,0,0,0];
+    timerRunning=false;
+    theTimer.innerHTML="00:00:00";
+    testArea.value="";
+    testwrapper.style.borderColor="grey";
+
+
+    
+}
+
+
+
+
 
 
 function Start(){
@@ -42,7 +94,7 @@ function Start(){
     if(textEnteredLength==0 && !timerRunning)
         {
 
-   setInterval(startTimer,10); 
+  interval = setInterval(startTimer,10); 
    timerRunning=true;   
 }
 
@@ -53,6 +105,8 @@ function Start(){
 
 
 testArea.addEventListener("keypress",Start);
+testArea.addEventListener("keyup",spellCheck);
+resetButton.addEventListener("click", reset);
 
 
 
